de4175249c37325cf20e32be08bd91a743f5e322
mithunreddy03 <mithunreddy3836@gmail.com>
2025-06-06 19:00:08 +0530

    update

--- P2/script.js
+++ P2/script.js
@@ -1,23 +1,32 @@
-const showLogin = () => {
+const showLogin=()=>{
     let str=`
     <div class='App-Container'>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
-    <p><button onclick='showRegister()'>Create Account</button></p>
+    <p><input type="text" id="txtEmail"></input></p>
+    <p><input type="password" id="txtPass"></input></p>
+    <p><button onclick="showHome()">Log In</button></p>
+    <p><button onclick="showRegister()">Create Account</button></p>
     </div>
     `
-    root.innerHTML = str
+    root.innerHTML=str 
 }
 
-const showRegister = () => {
-      let str=`
+const showRegister=()=>{
+    let str=`
+    <div class='App-Container'>
     <h1>Register Form</h1>
     <p><input type="text" id="txtName"></p>
     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
+    <p><input type="password" id=txtPass"></p>
     <button>Register</button>
+    <p><button onclick="showLogin()">Already a member? Login here...</button></p>
+    </div>
     `
-    root.innerHTML = str
+    root.innerHTML=str
+}
+const showHome=()=>{
+    let str=`
+    <h1>Welcome<h1>
+    `
+    root.innerHTML=str
 }
