c23a31d52b51c0bbdbaeda81e981952f1475a69f
mithunreddy03 <mithunreddy3836@gmail.com>
2025-06-07 11:34:58 +0530

    update

--- P2/script.js
+++ P2/script.js
@@ -1,32 +1,95 @@
-const showLogin=()=>{
-    let str=`
-    <div class='App-Container'>
+const users = [];
+let user={}
+const showLogin = () => {
+  let str = `
+    <div>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></input></p>
-    <p><input type="password" id="txtPass"></input></p>
-    <p><button onclick="showHome()">Log In</button></p>
-    <p><button onclick="showRegister()">Create Account</button></p>
+    <p><div id="dvMsg"></div></p>
+    <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <p><button onclick='validateUser()'>Log In</button></p>
+    <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
-    `
-    root.innerHTML=str 
-}
+    `;
+  root.innerHTML = str;
+};
 
-const showRegister=()=>{
-    let str=`
-    <div class='App-Container'>
+const showRegister = () => {
+  let str = `
     <h1>Register Form</h1>
     <p><input type="text" id="txtName"></p>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id=txtPass"></p>
-    <button>Register</button>
-    <p><button onclick="showLogin()">Already a member? Login here...</button></p>
-    </div>
-    `
-    root.innerHTML=str
-}
-const showHome=()=>{
-    let str=`
-    <h1>Welcome<h1>
-    `
-    root.innerHTML=str
-}
+     <p><input type="text" id="txtEmail"></p>
+    <p><input type="password" id="txtPass"></p>
+    <button onclick='addUser()'>Register</button>
+    <hr>
+    <button onClick='showLogin()'>Alread a Member? Login here...</button>
+    `;
+  root.innerHTML = str;
+};
+
+const showHome = () => {
+  let str = `
+    <h1>Welcome ${user.name}</h1>
+    <hr>
+    <p>
+      <select id="action">
+        <option value="0">--select--</option>
+        <option value="1">Deposit</option>
+        <option value="2">Withdraw</option>
+      </select>
+    </p>
+    <p>
+      <input type='number' id='txtAmount'>
+    </p>
+    <p>
+      <button onclick="performTransaction()">Submit</button>
+      <button onclick='showLogin()'>Logout</button>
+    </p>
+    <hr>
+    <p>Current balance: ${user.balance}</p>
+  `;
+  root.innerHTML = str;
+};
+
+
+const addUser = () => {
+  const obj = {
+    name: document.getElementById("txtName").value,
+    email: document.getElementById("txtEmail").value,
+    pass: document.getElementById("txtPass").value,
+    balance:0
+  };
+  users.push(obj);
+  showLogin();
+};
+
+const validateUser = () => {
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+   user = users.find(
+    (e) => e.email === email && e.pass === pass
+  )
+  if (user) {
+    showHome();
+  } else {
+    dvMsg.innerHTML = "Access Denied";
+  }
+};
+
+const performTransaction=() => {
+    const action=document.getElementById("action").value;
+    const amount=Number(document.getElementById("txtAmount").value);
+    if(action==1) {
+        user.balance+=amount;
+    }
+    else if(action==2) {
+        if(amount<=user.balance) {
+            user.balance-=amount;
+        }
+        else {
+            alert("Insufficient balance");
+            return;
+        }
+    }
+    showHome();
+};
